d61fd5940d89b8f97f02bc45975be41dbc142941
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-02-02 23:26:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Infinityworl/alive /alive
+  git clone https://github.com/Infinityworl/alive.git /alive
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /alive
